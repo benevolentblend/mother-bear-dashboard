32ea49704105fae203ef319ee84f8aec5f7f131f
Benjamin Loong Thomas <benjamin@thomasnetwork.net>
2017-10-21 17:35:16 -0400

    v1.0.0

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+/************************************************************
+ * Author:          Benjamin Thomas
+ * Major:           Software Development
+ * Creation Date:   October 21, 2017
+ * Due Date:        TBD
+ * Course:          CSC354 010
+ * Professor Name:  Dr. Hussain
+ * Filename:        main.js
+ * Purpose:         Main application file that loads the web page
+/************************************************************/
+
 const electron = require('electron');
 const {app, BrowserWindow} = electron;
 
 // When ready, create the main window
 app.on('ready', () => {
   let win = new BrowserWindow();
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadURL(`file://${__dirname}/resources/index.html`);
 });
 
 // When all windows have been closed, quit the application
---
+++ resources/js/app/app.module.js
@@ -0,0 +1,16 @@
+/************************************************************
+ * Author:          Benjamin Thomas
+ * Major:           Software Development
+ * Creation Date:   October 21, 2017
+ * Due Date:        TBD
+ * Course:          CSC354 010
+ * Professor Name:  Dr. Hussain
+ * Filename:        resources/app/app.module.js
+ * Purpose:         main angular module set sets up panel configurations
+/************************************************************/
+
+
+(function(){
+  angular
+    .module('app', [])
+})();
