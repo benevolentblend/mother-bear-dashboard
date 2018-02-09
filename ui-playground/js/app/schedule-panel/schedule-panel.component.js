52b5f805809b5624c0b0161bae8de9f600623246
Benjamin Loong Thomas <benjamin@thomasnetwork.net>
2018-02-09 12:35:18 -0500

    added ui playground

---
+++ ui-playground/js/app/schedule-panel/schedule-panel.component.js
@@ -0,0 +1,83 @@
+/**
+ * Name:    js/app/calendar-info/calendar-info.component.js
+ * Author:  Benjamin Thomas
+ * Purpose: Component for the panels
+ */
+
+(function() {
+  var calendarInfo = {
+    templateUrl: 'js/app/schedule-panel/schedule-panel.html',
+    controller: calendarInfoController
+  };
+
+
+  function calendarInfoController($interval, $http) {
+    var vm = this;
+    var timeoutid;
+    var location = '/calendar/benjamin@thomasnetwork.net';
+
+    vm.response = undefined;
+
+    function success(res) {
+      vm.response = res.data;
+      console.log(res.data);
+    }
+
+    function fail(res) {
+      vm.response = undefined;
+    }
+
+    function update() {
+      var dumpydata = {
+        data: {
+          events:[{
+            "id": 123,
+            "summary": "CSC 328",
+            "start": {
+              "dateTime": "2017-10-21T08:00:00-04:00"
+            },
+            "end": {
+              "dateTime": "2017-10-21T09:20:00-04:00"
+            },
+          },
+          {
+            "id": 126,
+            "summary": "TVR 201",
+            "start": {
+              "dateTime": "2017-10-21T09:30:00-04:00"
+            },
+            "end": {
+              "dateTime": "2017-10-21T11:00:00-04:00"
+            },
+          },
+          {
+            "id": 124,
+            "summary": "CSC 354",
+            "start": {
+              "dateTime": "2017-10-21T13:30:00-04:00"
+            },
+            "end": {
+              "dateTime": "2017-10-21T15:00:00-04:00"
+            },
+          }]
+        }
+      }
+      success(dumpydata);
+      //$http.get(location, {cache: false}).then(success, fail);
+    }
+
+    timeoutid = $interval(update, 5 * 60 * 1000);
+
+    vm.$onDestroy = function() {
+      $interval.cancel(timeoutid);
+    }
+
+    update();
+  }
+
+  calendarInfoController.$inject = ['$interval', '$http'];
+
+  angular
+    .module('app')
+    .component('schedulePanel', calendarInfo);
+})();
---
+++ ui-playground/js/app/weather-panel/weather-panel.component.js
@@ -0,0 +1,84 @@
+/**
+ * Name:    js/app/weather-info/weather-info.component.js
+ * Author:  Benjamin Thomas
+ * Purpose: Component for weather info panel
+ */
+
+(function() {
+  /**
+   * Controller for weatherInfo component. Handles the http requests as well as
+   * refreshing the content
+   * @method weatherInfoController
+   * @param  {module}              $interval angular module to handle interval
+   *                                         calls
+   * @param  {module}              $http     angular module to handle http
+   *                                         requests
+   * @return {}                              None
+   */
+  function weatherInfoController($interval, $http) {
+    var vm = this;
+    var timeoutid;
+    var location = 'http://api.openweathermap.org/data/2.5/weather';
+    var apikey = 'ddd84a4441bdebf3df5df4140cae463a';
+    var q = 'Kutztown';
+    var units = 'imperial';
+    var uri = location + '?apikey=' + apikey + '&q=' + q + '&units=' + units;
+    vm.response = undefined;
+
+    /**
+     * successful http callback
+     * @method success
+     * @param  {object} res contains the response from the http request
+     * @return {[   ]}     None
+     */
+    function success(res) {
+      vm.response = res.data;
+      console.log(res.data);
+    }
+
+    /**
+     * failing http callback
+     * @method fail
+     * @param  {object} res contains the response from the http request
+     * @return {[   ]}     None
+     */
+    function fail(res) {
+      vm.response = undefined;
+    }
+
+    /**
+     * http request that calls success or fail, depending on if the request
+     * was successful
+     * @method update
+     * @return {} None
+     */
+    function update() {
+      $http.get(uri,{cache: false}).then(success, fail);
+    }
+
+    // Setup interval call on update every 5 minutes (in milliseconds)
+    timeoutid = $interval(update, 5 * 60 * 1000);
+
+    // If the component is destroyed, stop the interval call
+    vm.$onDestroy = function() {
+      $interval.cancel(timeoutid);
+    }
+
+    // Call update for the first time
+    update();
+  }
+
+  // If this file is complied with a minifier program, insure that the correct
+  // modules are injected
+  weatherInfoController.$inject = ['$interval', '$http'];
+
+  // Setup the weatherInfo component with the templateUrl and controller
+  var weatherInfo = {
+    templateUrl: 'js/app/weather-panel/weather-panel.html',
+    controller: weatherInfoController
+  };
+
+  angular
+    .module('app')
+    .component('weatherPanel', weatherInfo);
+})();
